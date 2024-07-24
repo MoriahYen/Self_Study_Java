503216cab4f4b3ff38edd95bef97f6c211ddcb5a
MoriahYen <moriahyen@gmail.com>
2024-07-24 20:41:39 +0800

    [Telusko] Crud operations.

--- JDBCCourse/src/main/java/DemoJdbc.java
+++ JDBCCourse/src/main/java/DemoJdbc.java
@@ -4,23 +4,21 @@ public class DemoJdbc {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "0000";
-        String sql = "select * from student";
+        //String sql = "insert into student values (5,'John', 48)";
+        //String sql = "update student set sname = 'Max' where sid = 5";
+        String sql = "delete from student where sid = 5";
+
 
         //Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
         System.out.println("Connection Established");
         Statement st= con.createStatement();
-        ResultSet rs = st.executeQuery(sql); // executeQuery不會改變DB的東西
-//        System.out.println(rs.next());  // 有下一行data就會return T
-//        rs.next();  // 不然pointer或指向first row之前
-//        String name = rs.getString("sname");
-//        System.out.println(name);
-
-        while(rs.next()) {
-            System.out.print(rs.getInt(1) + " - ");
-            System.out.print(rs.getString(2) + " - ");
-            System.out.println(rs.getInt(3));
-        }
+        //boolean status =
+        st.execute(sql);
+        // 看敘述，的確是return F
+        // Returns: true if the first result is a ResultSet object;
+        // false if it is an update count or there are no results
+        //System.out.println(status);
 
         con.close();
         System.out.println("Connection Closed");
