361adceb41911c25b58d0ff3333d5ca41e07e269
MoriahYen <moriahyen@gmail.com>
2024-07-24 20:54:50 +0800

    [Telusko] 126. Problems with Statement.

--- JDBCCourse/src/main/java/DemoJdbc.java
+++ JDBCCourse/src/main/java/DemoJdbc.java
@@ -1,12 +1,16 @@
 import java.sql.*;
 public class DemoJdbc {
     public static void main(String[] args) throws Exception {
+        int sid = 101;
+        String sname = "Max";
+        int marks = 48;
+
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String pass = "0000";
         //String sql = "insert into student values (5,'John', 48)";
         //String sql = "update student set sname = 'Max' where sid = 5";
-        String sql = "delete from student where sid = 5";
+        String sql = "insert into student values (" + sid + ", '" + sname +"', " + marks + ")";
 
 
         //Class.forName("org.postgresql.Driver");
