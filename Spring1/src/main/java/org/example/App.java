e91e6c3a6330352c046c5a14f4c44069178c107d
MoriahYen <moriahyen@gmail.com>
2024-07-30 10:47:41 +0800

    [Telusko] 148. Getbean By Type.

--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -11,9 +11,12 @@ public class App
         // line15就已經create obj
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         // line18 using obj
-        Alien obj1 = (Alien) context.getBean("alien1");
+        Alien obj1 = context.getBean("alien1", Alien.class);
         //obj1.setAge(21);
         System.out.println(obj1.getAge());
         obj1.code();
+
+        Computer com = context.getBean(Computer.class);
+        Desktop obj = context.getBean(Desktop.class);
     }
 }
