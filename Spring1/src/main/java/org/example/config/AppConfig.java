d698367e3819961619968e78b9ccd0b9072cd3c6
MoriahYen <moriahyen@gmail.com>
2024-07-30 16:56:23 +0800

    [Telusko] 155. Component Stereotype Annotation.

--- Spring1/src/main/java/org/example/config/AppConfig.java
+++ Spring1/src/main/java/org/example/config/AppConfig.java
@@ -6,31 +6,29 @@ import org.example.Desktop;
 import org.example.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
 
 @Configuration
+@ComponentScan("org.example")
 public class AppConfig {
-    @Bean
-    public Alien alien(/*@Autowired*/ /*@Qualifier("desktop")*/ Computer com) {
-        Alien obj = new Alien();
-        obj.setAge(25);
-        obj.setCom(com);
-        return obj;
-    }
-
-    //@Bean(name = {"com2", "desktop1", "beast"})
-    @Bean
-    //@Scope("prototype")
-    public Desktop desktop() {
-        return new Desktop();
-    }
-
-    @Bean
-    @Primary
-    public Laptop laptop() {
-        return new Laptop();
-    }
+//    @Bean
+//    public Alien alien(/*@Autowired*/ /*@Qualifier("desktop")*/ Computer com) {
+//        Alien obj = new Alien();
+//        obj.setAge(25);
+//        obj.setCom(com);
+//        return obj;
+//    }
+//
+//    //@Bean(name = {"com2", "desktop1", "beast"})
+//    @Bean
+//    //@Scope("prototype")
+//    public Desktop desktop() {
+//        return new Desktop();
+//    }
+//
+//    @Bean
+//    @Primary
+//    public Laptop laptop() {
+//        return new Laptop();
+//    }
 }
