68bb60467917cd7fbb0f2bf6867cd5ccc5cae7d1
MoriahYen <moriahyen@gmail.com>
2024-07-30 11:10:44 +0800

    [Telusko] 150. Java Based Config.

--- Spring1/src/main/java/org/example/App.java
+++ Spring1/src/main/java/org/example/App.java
@@ -1,20 +1,27 @@
 package org.example;
 
+import org.example.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // create container，再透過xml create obj
-        // line15就已經create obj
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        // line18 using obj
-        Alien obj1 = context.getBean("alien1", Alien.class);
-        //obj1.setAge(21);
-        System.out.println(obj1.getAge());
-        obj1.code();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
+
+
+//        // create container，再透過xml create obj
+//        // line15就已經create obj
+//        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        // line18 using obj
+//        Alien obj1 = context.getBean("alien1", Alien.class);
+//        //obj1.setAge(21);
+//        System.out.println(obj1.getAge());
+//        obj1.code();
 
 //        Computer com = context.getBean(Computer.class);
 //        Desktop obj = context.getBean(Desktop.class);
