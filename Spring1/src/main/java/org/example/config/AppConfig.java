81696e52a3b59e1669d9a67c53fb493bc742c8e8
MoriahYen <moriahyen@gmail.com>
2024-07-30 16:44:10 +0800

    [Telusko] 154. Primary And Qualifier.

--- Spring1/src/main/java/org/example/config/AppConfig.java
+++ Spring1/src/main/java/org/example/config/AppConfig.java
@@ -3,15 +3,18 @@ package org.example.config;
 import org.example.Alien;
 import org.example.Computer;
 import org.example.Desktop;
+import org.example.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public Alien alien(/*@Autowired*/ Computer com) {
+    public Alien alien(/*@Autowired*/ /*@Qualifier("desktop")*/ Computer com) {
         Alien obj = new Alien();
         obj.setAge(25);
         obj.setCom(com);
@@ -23,5 +26,11 @@ public class AppConfig {
     //@Scope("prototype")
     public Desktop desktop() {
         return new Desktop();
+    }
+
+    @Bean
+    @Primary
+    public Laptop laptop() {
+        return new Laptop();
     }
 }
