fee13b8ad47cb2e1179eeb6ca884cb714e55da1e
MoriahYen <moriahyen@gmail.com>
2024-07-31 18:28:38 +0800

    [Telusko] 167. JDBCtemplate.

--- SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/repo/StudentRepo.java
+++ SpringJDBCEx/src/main/java/com/example/SpringJDBCEx/repo/StudentRepo.java
@@ -1,6 +1,8 @@
 package com.example.SpringJDBCEx.repo;
 
 import com.example.SpringJDBCEx.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,8 +10,23 @@ import java.util.List;
 
 @Repository
 public class StudentRepo {
+    private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
     public void save(Student s) {
-        System.out.println("Added");
+        String sql = "insert into student (rollNo, name, marks) values(?, ?, ?)";
+
+        int rows = jdbc.update(sql, s.getRollNo(), s.getName(), s.getClass());
+        System.out.println(rows + "effected");
+
     }
 
     public List<Student> findAll() {
