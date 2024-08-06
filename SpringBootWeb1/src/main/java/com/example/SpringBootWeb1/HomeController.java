5894ed7bfc6fda8d2147f06736ce2507c17629e6
MoriahYen <moriahyen@gmail.com>
2024-08-06 20:34:44 +0800

    [Telusko] 186. Setting Prefix and Suffix.

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -3,10 +3,9 @@ package com.example.SpringBootWeb1;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import java.sql.SQLOutput;
 
 @Controller
 public class HomeController {
@@ -14,18 +13,18 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         System.out.println("home method called");
-        return "index.jsp";
+        return "index";
     }
 
     @RequestMapping("add")
     // Spring is responsible to assign HttpServletRequest
-    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, HttpSession session) {
+    public String add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, Model model) {
         // http://localhost:8080/add?num1=6&num2=7
         // 因為paramaters的名稱和URL中的相同，所以可以直接用
         int result = num1 + num2;
 
-        session.setAttribute("result", result);
+        model.addAttribute("result",result);
 
-        return "result.jsp";
+        return "result";
     }
 }
--- SpringBootWeb1/src/main/webapp/views/result.jsp
+++ SpringBootWeb1/src/main/webapp/views/result.jsp
@@ -6,10 +6,10 @@
     </head>
     </body>
         <!-- using plain Java -->
-        <h2>Result is: <%= session.getAttribute("result") %></h2>
+<%--        <h2>Result is: <%= session.getAttribute("result") %></h2>--%>
 
         <!--  using jstl tags -->
-<%--        <h2>Result is: ${result}  </h2>--%>
+        <h2>Result is: ${result}  </h2>
 
     </body>
 </html>
