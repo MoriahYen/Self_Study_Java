d73ca85947a8504b44504a7927503834049c07da
MoriahYen <moriahyen@gmail.com>
2024-08-06 18:56:55 +0800

    [Telusko] 183. Display Data on Result Page.

--- SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
+++ SpringBootWeb1/src/main/java/com/example/SpringBootWeb1/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.SpringBootWeb1;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,5 +14,17 @@ public class HomeController {
     public String home() {
         System.out.println("home method called");
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    // Spring is responsible to assign HttpServletRequest
+    public String add(HttpServletRequest req, HttpSession session) {
+        int num1 = Integer.parseInt(req.getParameter("num1"));
+        int num2 = Integer.parseInt(req.getParameter("num2"));
+        int result = num1 + num2;
+
+        session.setAttribute("result", result);
+
+        return "result.jsp";
     }
 }
