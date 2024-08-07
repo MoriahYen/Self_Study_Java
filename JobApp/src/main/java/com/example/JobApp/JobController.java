0e7520cda3021c0196c0a7d1084a0c71a4cd2b45
MoriahYen <moriahyen@gmail.com>
2024-08-07 21:40:59 +0800

    [Telusko] 203. View Data.

--- JobApp/src/main/java/com/example/JobApp/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/JobController.java
@@ -4,8 +4,11 @@ import com.example.JobApp.model.JobPost;
 import com.example.JobApp.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -26,5 +29,12 @@ public class JobController {
     public String handleform(JobPost jobPost) {
         service.addJob(jobPost);
         return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model m) {
+        List<JobPost> jobs = service.getAllJobs();
+        m.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
     }
 }
