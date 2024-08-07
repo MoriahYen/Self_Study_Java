99d6f1a3e920efba1717252ba816b79714f17e33
MoriahYen <moriahyen@gmail.com>
2024-08-07 21:31:50 +0800

    [Telusko] 202. Working with Layers.

--- JobApp/src/main/java/com/example/JobApp/JobController.java
+++ JobApp/src/main/java/com/example/JobApp/JobController.java
@@ -1,12 +1,17 @@
 package com.example.JobApp;
 
 import com.example.JobApp.model.JobPost;
+import com.example.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class JobController {
+    @Autowired
+    private JobService service;
+
     @GetMapping({"/", "home"})
     public String home() {
         return "home";
@@ -19,6 +24,7 @@ public class JobController {
 
     @PostMapping("handleForm")
     public String handleform(JobPost jobPost) {
+        service.addJob(jobPost);
         return "success";
     }
 }
---
+++ JobApp/src/main/java/com/example/JobApp/service/JobService.java
@@ -0,0 +1,25 @@
+package com.example.JobApp.service;
+
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.repo.JobRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// send data to repo layer
+@Service
+public class JobService {
+    @Autowired
+    private JobRepo repo;
+
+    public void addJob(JobPost jobPost) {
+        repo.addJob(jobPost);
+    }
+
+    public List<JobPost> getAllJobs() {
+        return repo.getAllJobs();
+    }
+}
+
+
