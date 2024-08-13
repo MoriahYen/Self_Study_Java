e58b303a9c0218e5a1f7903abe7ecbd53dc622b1
MoriahYen <moriahyen@gmail.com>
2024-08-14 02:42:16 +0800

    [Telusko] 213. Sending Data and RequestBody.

--- SpringBootRest/src/main/java/com/telusko/SpringBootRest/JobRestController.java
+++ SpringBootRest/src/main/java/com/telusko/SpringBootRest/JobRestController.java
@@ -24,4 +24,10 @@ public class JobRestController {
     public JobPost getJob(@PathVariable("postId") int postId) {
         return service.getJob(postId);
     }
+
+    @PostMapping("jobPost")
+    public JobPost addJob(@RequestBody JobPost jobPost) {
+        service.addJob(jobPost);
+        return service.getJob(jobPost.getPostId());
+    }
 }
