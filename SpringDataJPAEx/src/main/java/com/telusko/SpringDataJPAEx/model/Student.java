e00d1d31eb62e4fedeb4d6273877241833742662
MoriahYen <moriahyen@gmail.com>
2024-08-14 17:16:44 +0800

    [Telusko] 218. Creating Table And Inserting Data.

---
+++ SpringDataJPAEx/src/main/java/com/telusko/SpringDataJPAEx/model/Student.java
@@ -0,0 +1,49 @@
+package com.telusko.SpringDataJPAEx.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+@Entity
+public class Student {
+    @Id // make rollNo as primary key
+    private int rollNo;
+    private String name;
+    private int marks;
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                '}';
+    }
+}
