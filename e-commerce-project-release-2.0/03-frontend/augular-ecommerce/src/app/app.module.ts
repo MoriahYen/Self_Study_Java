1d6147ca44833499596f31cc2853db8150e63a7c
moriahyen <moriahyen@gmail.com>
2024-10-07 18:31:49 +0800

    [e_commerce] 95. Angular Project - Search for Products by Keyword - Send Data to search route.

--- e-commerce-project-release-2.0/03-frontend/augular-ecommerce/src/app/app.module.ts
+++ e-commerce-project-release-2.0/03-frontend/augular-ecommerce/src/app/app.module.ts
@@ -8,9 +8,11 @@ import { ProductService } from './services/product.service';
 
 import { Routes, RouterModule} from '@angular/router';
 import { ProductCategoryMenuComponent } from './component/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   // [M] When path match, create new instance of component
+  {path: 'search/:keyword', component: ProductListComponent},
   {path: 'category/:id', component: ProductListComponent},
   {path: 'category', component: ProductListComponent},
   {path: 'products', component: ProductListComponent},
@@ -22,7 +24,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent
+    ProductCategoryMenuComponent,
+    SearchComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
